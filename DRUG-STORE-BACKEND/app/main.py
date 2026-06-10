b26ff1b2efe7f603f3dd285b65a035e258f65edc
dimon <238229712+demonstr32@users.noreply.github.com>
2026-06-10 14:33:49 +0300

    main

--- DRUG-STORE-BACKEND/app/main.py
+++ DRUG-STORE-BACKEND/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.routers.auth import router as auth_router
 from app.api.routers.prod import router as prod_router
+from app.api.routers.cart import router as cart_router
 
 app = FastAPI(title="Drug Store API", version="1.0.0")
 
@@ -17,7 +18,7 @@ app.add_middleware(
 
 app.include_router(auth_router)
 app.include_router(prod_router)
-
+app.include_router(cart_router)
 @app.get("/")
 def root():
     return {"message": "Drug Store API is running"}
