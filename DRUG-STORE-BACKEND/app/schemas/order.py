4bdb6998c101a13741ae9d0ebe51bb6f3f782413
dimon <238229712+demonstr32@users.noreply.github.com>
2026-06-12 22:47:28 +0300

    schemas

---
+++ DRUG-STORE-BACKEND/app/schemas/order.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from uuid import UUID
+from pydantic import BaseModel, ConfigDict
+class OrderCreateSchema(BaseModel):
+    pass
+class OrderItemResponseSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)    
+    id: int
+    product_id: int
+    product_name: str
+    price: float
+    quantity: int
+class OrderResponseSchema(BaseModel):
+    id: UUID
+    user_id: UUID
+    created_at: datetime
+    total_price: float
+    status: str
+    items: list[OrderItemResponseSchema]
+class OrderUpdateSchema(BaseModel):
+    status: str
